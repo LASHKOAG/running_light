7b9e6962fd134c99b5e3b3ebb88cf2866d421ce2
LASHKOAG <lashkoag@yandex.ru>
2022-09-18 10:47:36 +0600

    init varriables

--- running_lights/running_lights.ino
+++ running_lights/running_lights.ino
@@ -1,1 +1,19 @@
-//
+const int LED_WHITE = 5;
+const int LED_BLUE = 6;
+const int LED_YELLOY = 7;
+const int DELAY = 100;
+
+int Blink(int pin){
+  delay(DELAY);
+  digitalWrite(pin, HIGH);
+  delay(DELAY);
+  digitalWrite(pin, LOW);
+  delay(DELAY);
+}
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  LongBlink(PIN);
+}
