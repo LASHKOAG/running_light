341f34ca2161c28acd4a0c623af2c9dc1e02ea5e
LASHKOAG <lashkoag@yandex.ru>
2022-09-18 11:10:52 +0600

    oiuytiofhj

--- running_lights/running_lights.ino
+++ running_lights/running_lights.ino
@@ -2,13 +2,21 @@ const int LED_WHITE = 4;
 const int LED_RED = 5;
 const int LED_BLUE = 6;
 const int LED_YELLOY = 7;
-const int DELAY = 200;
+const int DELAY = 250;
 
 int Blink(int pin){
   delay(DELAY);
   digitalWrite(pin, HIGH);
   delay(DELAY);
   digitalWrite(pin, LOW);
+  //delay(DELAY);
+}
+int Blink(int pin, int pin2){
+//  delay(DELAY);/
+  digitalWrite(pin, HIGH);
+  delay(DELAY);
+  digitalWrite(pin, LOW);
+  digitalWrite(pin2, HIGH);
   //delay(DELAY);
 }
 void setup() {
@@ -20,6 +28,7 @@ void setup() {
 
 void loop() {
   for (int i = 4; i < 8; ++i){
-    Blink(i);
+//    Blink(i);
+      Blink(i, i+1);
   }
 }
